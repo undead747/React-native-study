08cea976bfce2ae3dd82ce8acca3be0eb3285e58
unknown <minh.trancong@NSK.LOCAL>
2023-09-19 13:01:31 +0900

    init popular job card

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,17 +1,40 @@
 import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, ActivityIndicator, FlatList } from 'react-native'
 
 import styles from './popularjobs.style'
+import { COLORS, SIZES } from '../../../constants';
+import PopularjobCard from '../../common/cards/popular/PopularJobCard';
 
 const Popularjobs = () => {
+  const isLoading = false;
+  const error = false;
+
   return (
     <View style={styles.container}>
-        <View style={styles.header}>
-          <Text style={styles.headerTitle}>Popular jobs</Text>
-          <TouchableOpacity>
-              <Text style={styles.headerBtn}>Show all</Text>
-          </TouchableOpacity>
-        </View>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {
+          isLoading ? (
+            <ActivityIndicator size={'large'} colors={COLORS.primary} />) :
+            error ? (
+              <Text>Something went wrongs</Text>) :
+              <FlatList
+                data={[1, 2, 3, 4, 5]}
+                renderItem={({item}) => (
+                  <PopularjobCard item={item} />
+                )}
+                keyExtractor={item => item?.job_id}
+                contentContainerStyle={{columnGap: SIZES.medium}}
+                horizontal
+              />
+        }
+      </View>
     </View>
   )
 }
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -49,6 +49,7 @@ const Welcome = () => {
           }
           keyExtractor={item => item}
           contentContainerStyle={{columnGap: SIZES.small}}
+          horizontal
         /> 
       </View>
     </View>
